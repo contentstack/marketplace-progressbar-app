7acec2a061019bda20d0480c8b585496743bad6c
Hanoak <hanoak.suchethan@contentstack.com>
2022-02-03 13:32:34 +0530

    cleaned customField component

--- ui/src/containers/CustomField/index.spec.tsx
+++ ui/src/containers/CustomField/index.spec.tsx
@@ -3,22 +3,16 @@ import { render, waitFor } from "@testing-library/react/pure";
 import CustomField from "./index";
 
 let customFieldDom: any;
+const elementsToTest = [".customField", ".customField__slide_bar"];
+
 beforeAll(async () => {
   customFieldDom = render(<CustomField />);
 });
 
-describe(`UI Elements of Custom Field Screen`, () => {
-  test(`Layout container element test`, async () => {
+elementsToTest.forEach((e) => {
+  test(`Testing ${e} element`, async () => {
     await expect(
-      waitFor(() =>
-        customFieldDom?.container.querySelector(".layout-container")
-      )
-    ).toBeTruthy();
-  });
-
-  test(`progress bar element test`, async () => {
-    await expect(
-      waitFor(() => customFieldDom?.container.querySelector(".slideBarDiv"))
+      waitFor(() => customFieldDom?.container?.querySelector(e))
     ).toBeTruthy();
   });
 });
--- ui/src/containers/CustomField/index.tsx
+++ ui/src/containers/CustomField/index.tsx
@@ -5,30 +5,31 @@ import Slider, { SliderProps } from "@mui/material/Slider";
 import { alpha, styled } from "@mui/material/styles";
 import { isEmpty } from "lodash";
 
-import { TypeDataSDK, ProgressBarData } from "../../common/types";
-import "./styles.scss";
-import "./index.css";
+import { TypeSDKData, TypeProgressBar } from "../../common/types";
+import "./styles.css";
+
+const sliderColor = "#5d50bf";
 
 const SuccessSlider = styled(Slider)<SliderProps>(() => ({
-  color: "#5d50bf",
+  color: sliderColor,
   "& .MuiSlider-thumb": {
     "&:hover, &.Mui-focusVisible": {
-      boxShadow: `0px 0px 0px 8px ${alpha("#5d50bf", 0.16)}`,
+      boxShadow: `0 0 0 8 ${alpha(sliderColor, 0.16)}`,
     },
     "&.Mui-active": {
-      boxShadow: `0px 0px 0px 14px ${alpha("#5d50bf", 0.16)}`,
+      boxShadow: `0 0 0 14 ${alpha(sliderColor, 0.16)}`,
     },
   },
 }));
 
 const CustomField: React.FC = function () {
-  const [state, setState] = useState<TypeDataSDK>({
+  const [state, setState] = useState<TypeSDKData>({
     config: {},
     location: {},
     appSdkInitialized: false,
   });
 
-  const [slideValue, setSlideValue] = useState<[ProgressBarData]>([
+  const [slideValue, setSlideValue] = useState<[TypeProgressBar]>([
     {
       value: 10,
     },
@@ -58,12 +59,11 @@ const CustomField: React.FC = function () {
   };
 
   return (
-    <div className="layout-container">
+    <div className="layout-container customField">
       {state.appSdkInitialized && (
-        <div className="slideBarDiv">
+        <div className="customField__slide_bar">
           <SuccessSlider
             defaultValue={10}
-            aria-label="Default"
             valueLabelDisplay="auto"
             onChange={onChangeSave}
             value={slideValue[0].value}
